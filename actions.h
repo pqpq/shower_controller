f054bb63fec78bb5a2db5e0a9b6cfeadbaeff550
mrj <m@badcrumble.net>
2017-09-29 21:14:12 +0100

    add all other actions I can think of

--- actions.h
+++ actions.h
@@ -9,14 +9,19 @@ public:
     virtual void valveOpen() = 0;
 
     virtual void greenLedOn() = 0;
+    virtual void greenLedOff() = 0;
     virtual void greenLedFlashing() = 0;
+    virtual void redLedFlashing() = 0;
 
     virtual void showShowerTime() = 0;
+    virtual void countDownTime() = 0;
+    virtual void countDownLockout() = 0;
     virtual void displayDim() = 0;
     virtual void displayBright() = 0;
 
     virtual void shortBeep() = 0;
     virtual void longBeep() = 0;
+    virtual void rapidBeep() = 0;
 
     virtual void coldTimerStart() = 0;
     virtual void coldTimerStop() = 0;
