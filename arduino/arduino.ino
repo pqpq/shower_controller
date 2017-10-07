e6047b970280e005fd313b7018987912d726dd44
mrj <m@badcrumble.net>
2017-10-07 16:18:14 +0100

    #define for all pins

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,53 +1,41 @@
-void write7seg(int);
 
+#define SEG_A         (3)
+#define SEG_B         (4)
+#define SEG_C         (0)
+#define SEG_D         (1)
+#define SEG_E         (6)
+#define SEG_F         (2)
+#define SEG_G         (5)
 
-// the setup function runs once when you press reset or power the board
-void setup() 
-{
-  pinMode(LED_BUILTIN, OUTPUT);
+#define LATCH0_ENABLE (7)
+#define LATCH1_ENABLE (8)
 
-  // 7-seg data lines
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+#define LEDS_OE       (9)
 
-  // latch enables
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-
-  write7seg(0);
-
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-}
-
-//      3
-//    *****
-//   *     *
-// 2 *     * 4
-//   *     *
-//    *****
-//   *  5  *
-// 6 *     * 0
-//   *     *
-//    *****
-//      1
+//       a/3
+//      *****
+//     *     *
+// f/2 *     * b/4
+//     *     *
+//      *****
+//     * g/5 *
+// e/6 *     * c/0
+//     *     *
+//      *****
+//       d/1
 const int digitMapping[10] =
 {
-  0b1011111,    // 0
-  0b0010001,    // 1
-  0b1111010,    // 2
-  0b0111011,    // 3
-  0b0110101,    // 4
-  0b0101111,    // 5
-  0b1101111,    // 6
-  0b0011001,    // 7
+  //gfedcba
+  0b0111111,    // 0
+  0b0000110,    // 1
+  0b1011011,    // 2
+  0b1001111,    // 3
+  0b1100110,    // 4
+  0b1101101,    // 5
+  0b1111101,    // 6
+  0b0000111,    // 7
   0b1111111,    // 8
-  0b0111111     // 9
+  0b1101111     // 9
 };
 
 //     _  _     _  _     _  _     _  _     _  _     _  _ 
@@ -60,24 +48,24 @@ const int spinnerMapping[]=
 
 void write7seg(int bits)
 {
-  digitalWrite(0, bits & 0b0000001 ? LOW : HIGH);
-  digitalWrite(1, bits & 0b0000010 ? LOW : HIGH);
-  digitalWrite(2, bits & 0b0000100 ? LOW : HIGH);
-  digitalWrite(3, bits & 0b0001000 ? LOW : HIGH);
-  digitalWrite(4, bits & 0b0010000 ? LOW : HIGH);
-  digitalWrite(5, bits & 0b0100000 ? LOW : HIGH);
-  digitalWrite(6, bits & 0b1000000 ? LOW : HIGH);
+  digitalWrite(SEG_A, bits & 0b0000001 ? LOW : HIGH);
+  digitalWrite(SEG_B, bits & 0b0000010 ? LOW : HIGH);
+  digitalWrite(SEG_C, bits & 0b0000100 ? LOW : HIGH);
+  digitalWrite(SEG_D, bits & 0b0001000 ? LOW : HIGH);
+  digitalWrite(SEG_E, bits & 0b0010000 ? LOW : HIGH);
+  digitalWrite(SEG_F, bits & 0b0100000 ? LOW : HIGH);
+  digitalWrite(SEG_G, bits & 0b1000000 ? LOW : HIGH);
 }
 
 void writeDigit(int digit, int value)
 {
   if (digit == 0)
   {
-    digitalWrite(7, HIGH);
+    digitalWrite(LATCH0_ENABLE, HIGH);
   }
   else
   {
-    digitalWrite(8, HIGH);
+    digitalWrite(LATCH1_ENABLE, HIGH);
   }
 
   if (digit == 1 && value == 0)
@@ -89,15 +77,54 @@ void writeDigit(int digit, int value)
     write7seg(digitMapping[value]);
   }
   
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
+  digitalWrite(LATCH0_ENABLE, LOW);
+  digitalWrite(LATCH1_ENABLE, LOW);
 
-  write7seg(0b0100000);
+  //write7seg(0b0100000);
 }
 
-  int n = 0;
+void displayDim()
+{
+  // ~OE so high PWM duty cycle = low duty cycle for LEDs
+  analogWrite(LEDS_OE, 220);
+}
 
-// the loop function runs over and over again forever
+void displayBright()
+{
+  digitalWrite(LEDS_OE, LOW);
+}
+
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // 7-seg data lines
+  pinMode(SEG_A, OUTPUT);
+  pinMode(SEG_B, OUTPUT);
+  pinMode(SEG_C, OUTPUT);
+  pinMode(SEG_D, OUTPUT);
+  pinMode(SEG_E, OUTPUT);
+  pinMode(SEG_F, OUTPUT);
+  pinMode(SEG_G, OUTPUT);
+
+  // latch enables
+  pinMode(LATCH0_ENABLE, OUTPUT);
+  pinMode(LATCH1_ENABLE, OUTPUT);
+
+  // output enable (LED dimmer)
+  pinMode(LEDS_OE, OUTPUT);
+
+  write7seg(0);
+
+  digitalWrite(LATCH0_ENABLE, LOW);
+  digitalWrite(LATCH1_ENABLE, LOW);
+  digitalWrite(LEDS_OE, LOW);
+}
+
+
+int n = 0;
+
 void loop()
 {
   writeDigit(0, n % 10);
@@ -108,7 +135,17 @@ void loop()
     n = 0;
   }
 
-  digitalWrite(LED_BUILTIN, n % 2 ? HIGH : LOW);
+  // demonstrate PWM dimming
+  if (n/10 % 2)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    displayDim();
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    displayBright();
+  }
 
   delay(100);
 }
