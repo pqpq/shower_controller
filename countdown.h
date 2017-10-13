6b457444d23d9fd07f82946732791700cec8a15e
mrj <m@badcrumble.net>
2017-10-13 19:44:15 +0100

    Finished Countdown class

--- countdown.h
+++ countdown.h
@@ -1,5 +1,81 @@
 
+/// Deals with the logic of time passing, as we count down the minutes and seconds.
+/// The various "time has passed" methods should be called at regular intervals,
+/// and this class decides whether they trigger callbacks.
+///
 class Countdown
 {
+public:
+    typedef unsigned int Seconds;
+    typedef Seconds TimeToGoFn();
+    typedef void EventCallback();
 
+    Countdown(TimeToGoFn & timeToGo,
+            EventCallback & fiveMinutesCallback,
+            EventCallback & oneMinuteCallback,
+            EventCallback & fiveSecondCallback,
+            EventCallback & oneSecondCallback)
+    : timeToGo(timeToGo)
+    , fiveMinutesCallback(fiveMinutesCallback)
+    , lastTime5Mins(0)
+    , oneMinuteCallback(oneMinuteCallback)
+    , lastTime1Min(0)
+    , fiveSecondsCallback(fiveSecondCallback)
+    , oneSecondCallback(oneSecondCallback)
+    {}
+
+    void start()
+    {
+        const Seconds timeNow = timeToGo();
+        lastTime5Mins = timeNow;
+        lastTime1Min = timeNow;
+    }
+
+    void fiveMinutes()
+    {
+        const Seconds timeNow = timeToGo();
+        if (timeNow < 300 && lastTime5Mins >= 300)
+        {
+            fiveMinutesCallback();
+        }
+
+        lastTime5Mins = timeNow;
+    }
+
+    void oneMinute()
+    {
+        const Seconds timeNow = timeToGo();
+        if (timeNow < 60 && lastTime1Min >= 60)
+        {
+            oneMinuteCallback();
+        }
+
+        lastTime1Min = timeNow;
+    }
+
+    void fiveSeconds()
+    {
+        if (timeToGo() < 60)
+        {
+            fiveSecondsCallback();
+        }
+    }
+
+    void oneSecond()
+    {
+        if (timeToGo() < 10)
+        {
+            oneSecondCallback();
+        }
+    }
+
+private:
+
+    TimeToGoFn & timeToGo;
+    EventCallback & fiveMinutesCallback;
+    Seconds lastTime5Mins;
+    EventCallback & oneMinuteCallback;
+    Seconds lastTime1Min;
+    EventCallback & fiveSecondsCallback;
+    EventCallback & oneSecondCallback;
 };
--- test/countdown_test.cpp
+++ test/countdown_test.cpp
@@ -3,7 +3,155 @@
 
 #include "catch.hpp"
 
-TEST_CASE("Countdown class exists")
+Countdown::Seconds testTimeToGo = 0;
+Countdown::Seconds testGetTimeToGo()
 {
-    const Countdown uut;
+    return testTimeToGo;
+}
+
+size_t fiveMinutesCalls = 0;
+void fiveMinutesCallback()
+{
+    fiveMinutesCalls++;
+}
+
+size_t oneMinuteCalls = 0;
+void oneMinuteCallback()
+{
+    oneMinuteCalls++;
+}
+
+size_t fiveSecondCalls = 0;
+void fiveSecondCallback()
+{
+    fiveSecondCalls++;
+}
+
+size_t oneSecondCalls = 0;
+void oneSecondCallback()
+{
+    oneSecondCalls++;
+}
+
+void ignoreCallback() {}
+
+
+TEST_CASE("fiveMinutes triggers callback once when time to go passes 300 seconds")
+{
+    Countdown uut(testGetTimeToGo, fiveMinutesCallback, ignoreCallback, ignoreCallback, ignoreCallback);
+
+    testTimeToGo = 1000;
+    uut.start();
+
+    fiveMinutesCalls = 0;
+    uut.fiveMinutes();
+    REQUIRE(fiveMinutesCalls == 0);
+
+    testTimeToGo = 300;
+    uut.fiveMinutes();
+    REQUIRE(fiveMinutesCalls == 0);
+
+    testTimeToGo = 299;
+    uut.fiveMinutes();
+    REQUIRE(fiveMinutesCalls == 1);
+
+    testTimeToGo = 10;
+    fiveMinutesCalls = 0;
+    uut.fiveMinutes();
+    REQUIRE(fiveMinutesCalls == 0);
+}
+
+TEST_CASE("oneMinute triggers callback once when time to go passes 60 seconds")
+{
+    Countdown uut(testGetTimeToGo, ignoreCallback, oneMinuteCallback, ignoreCallback, ignoreCallback);
+
+    testTimeToGo = 100;
+    uut.start();
+
+    oneMinuteCalls = 0;
+    uut.oneMinute();
+    REQUIRE(oneMinuteCalls == 0);
+
+    testTimeToGo = 60;
+    uut.oneMinute();
+    REQUIRE(oneMinuteCalls == 0);
+
+    testTimeToGo = 59;
+    uut.oneMinute();
+    REQUIRE(oneMinuteCalls == 1);
+
+    testTimeToGo = 10;
+    oneMinuteCalls = 0;
+    uut.oneMinute();
+    REQUIRE(oneMinuteCalls == 0);
+}
+
+TEST_CASE("oneMinute and fiveMinutes called together triggers 5 minutes if time passes 300")
+{
+    Countdown uut(testGetTimeToGo, fiveMinutesCallback, oneMinuteCallback, ignoreCallback, ignoreCallback);
+
+    testTimeToGo = 1000;
+    uut.start();
+
+    oneMinuteCalls = 0;
+    fiveMinutesCalls = 0;
+
+    testTimeToGo = 299;
+    uut.oneMinute();
+    uut.fiveMinutes();
+
+    REQUIRE(oneMinuteCalls == 0);
+    REQUIRE(fiveMinutesCalls == 1);
+}
+
+TEST_CASE("fiveSeconds only triggers callbacks in last 60 seconds")
+{
+    Countdown uut(testGetTimeToGo, ignoreCallback, ignoreCallback, fiveSecondCallback, ignoreCallback);
+
+    testTimeToGo = 100;
+    uut.start();
+
+    fiveSecondCalls = 0;
+    uut.fiveSeconds();
+    REQUIRE(fiveSecondCalls == 0);
+
+    testTimeToGo = 60;
+    uut.fiveSeconds();
+    REQUIRE(fiveSecondCalls == 0);
+
+    testTimeToGo = 59;
+    uut.fiveSeconds();
+    REQUIRE(fiveSecondCalls == 1);
+
+    testTimeToGo = 10;
+    fiveSecondCalls = 0;
+    uut.fiveSeconds();
+    REQUIRE(fiveSecondCalls == 1);
+}
+
+TEST_CASE("oneSecond only triggers callbacks in last 10 seconds")
+{
+    Countdown uut(testGetTimeToGo, ignoreCallback, ignoreCallback, fiveSecondCallback, oneSecondCallback);
+
+    testTimeToGo = 60;
+    uut.start();
+
+    oneSecondCalls = 0;
+    uut.oneSecond();
+    REQUIRE(oneSecondCalls == 0);
+
+    testTimeToGo = 10;
+    oneSecondCalls = 0;
+    uut.oneSecond();
+    REQUIRE(oneSecondCalls == 0);
+
+    testTimeToGo = 9;
+    oneSecondCalls = 0;
+    uut.oneSecond();
+    REQUIRE(oneSecondCalls == 1);
+
+    testTimeToGo = 4;
+    oneSecondCalls = 0;
+    uut.oneSecond();
+    REQUIRE(oneSecondCalls == 1);
 }
